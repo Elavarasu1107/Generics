6908831160284bb2f412ae187b92047d5d48e422
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-26 20:04:36 +0530

    [Elavarasu] UC1-Array of Int,Char,Double

---
+++ Generics/Generics/Generics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Generics
+    {
+        public void Print(int[] details)
+        {
+            foreach(var data in details)
+            {
+                Console.Write(data+" ");
+            }
+        }
+        public void Print(double[] details)
+        {
+            foreach (var data in details)
+            {
+                Console.Write(data + " ");
+            }
+        }
+        public void Print(char[] details)
+        {
+            foreach (var data in details)
+            {
+                Console.Write(data + " ");
+            }
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var log = NLog.LogManager.GetCurrentClassLogger();
+            log.Info("Running Generics Program");
+
+            int[] integer = { 1, 2, 3, 4, 5 };
+            double[] doubles = { 1.1, 1.2, 1.3, 1.4, 1.5 };
+            char[] character = { 'A', 'B', 'C', 'D', 'E' };
+
+            Generics getMethod = new Generics();
+            getMethod.Print(integer);
+            Console.WriteLine();
+            getMethod.Print(doubles);
+            Console.WriteLine();
+            getMethod.Print(character);
+        }
+    }
+}
